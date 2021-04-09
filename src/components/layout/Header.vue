44e07d3a94e6bafe06d7d2b3b6b42fb6dc636f73
kimjoongmin <82187845+kimjoongmin@users.noreply.github.com>
2021-04-09 17:22:57 +0900

    2 commit

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-navbar-brand href="/vue-hour">Front-end developer</b-navbar-brand>
+        <b-navbar-brand href="/vue-hour">Front-end developer2</b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-            <b-nav-item href="/About">About</b-nav-item>
-            <b-nav-item href="/About">About</b-nav-item>
-            <b-nav-item href="/About">About</b-nav-item>
-            <b-nav-item href="/About">About</b-nav-item>
+            <b-nav-item href="/vue-hour/About">About</b-nav-item>
+            <b-nav-item href="/vue-hour/About">About</b-nav-item>
+            <b-nav-item href="/vue-hour/About">About</b-nav-item>
+            <b-nav-item href="/vue-hour/About">About</b-nav-item>
             <b-nav-item href="#" disabled>Disabled</b-nav-item>
         </b-navbar-nav>
 
