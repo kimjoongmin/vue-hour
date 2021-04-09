7f4de7641a48bba961a5183be6cd629091d55ac7
kimjoongmin <82187845+kimjoongmin@users.noreply.github.com>
2021-04-09 17:10:23 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="wrap">
+      <Header />
+      <div id="container">
+        <router-view></router-view>
+      </div>
+      <Footer />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/layout/Header.vue'
+import Footer from './components/layout/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Footer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html,body{text-align:center;}
 </style>
---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Copyright @ kimjoongmin</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
